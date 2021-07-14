0bb3b77a3acef1e2eb663621a63b8e168f639052
Alexandre Desbiens <linkalex8@hotmail.com>
2021-07-14 19:46:32 -0400

    CI

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    polyfillDynamicImport: true
+    polyfillDynamicImport: true,
+    outDir: "public"
   },
   plugins: [vue()]
 })
