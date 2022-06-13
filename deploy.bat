45945130bd4ad72a3721664e66d5b9d9ffeddf0b
Alexandre Desbiens <linkalex8@hotmail.com>
2022-06-13 08:16:36 -0400

    Fix

--- deploy.bat
+++ deploy.bat
@@ -3,4 +3,4 @@ cd dist
 call git init
 call git add -A
 call git commit -m "deploy"
-call git push -f git@github.com:alphspirit/warframe-mastery.git master:gh-pages
+call git push -f git@github.com:AlphSpirit/warframe-mastery.git master:gh-pages
